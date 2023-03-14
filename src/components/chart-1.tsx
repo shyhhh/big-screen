05c6c712ecc918461866dccb3dde62c6d7f6612d
shyhhh <hhhtheshy@gmail.com>
2023-03-14 16:13:56 +0800

    fix data

--- src/components/chart-1.tsx
+++ src/components/chart-1.tsx
@@ -25,7 +25,7 @@ export const Chart1 = () => {
             lineStyle: { color: '#083B70' },
           },
           axisLabel: {
-            formatter(val: string) {
+            formatter(val) {
               if (val.length > 2) {
                 const array = val.split('')
                 array.splice(2, 0, '\n')
@@ -36,6 +36,7 @@ export const Chart1 = () => {
             },
           },
         },
+
         yAxis: {
           splitLine: { show: false },
           axisLine: {
--- src/components/chart-3.tsx
+++ src/components/chart-3.tsx
@@ -5,9 +5,37 @@ import { px } from '../shared/px'
 
 export const Chart3 = () => {
   const divRef = useRef(null)
+  const myChart = useRef(null)
+  const data = [
+    { name: '2015', h1: 0.01, h2: 0.05, h3: 0.09, h4: 0.09, h5: 0.02 },
+    { name: '2016', h1: 0.02, h2: 0.04, h3: 0.08, h4: 0.13, h5: 0.11 },
+    { name: '2017', h1: 0.03, h2: 0.02, h3: 0.07, h4: 0.15, h5: 0.09 },
+    { name: '2018', h1: 0.04, h2: 0.09, h3: 0.06, h4: 0.06, h5: 0.01 },
+    { name: '2019', h1: 0.11, h2: 0.13, h3: 0.04, h4: 0.03, h5: 0.03 },
+    { name: '2020', h1: 0.22, h2: 0.09, h3: 0.05, h4: 0.02, h5: 0.07 },
+    { name: '2021', h1: 0.2,  h2: 0.03, h3: 0.07, h4: 0.01, h5: 0.08 },
+    { name: '2022', h1: 0.08, h2: 0.01, h3: 0.09, h4: 0.03, h5: 0.03 },
+    { name: '2023', h1: 0.01, h2: 0.02, h3: 0.01, h4: 0.13, h5: 0.18 },
+  ]
   useEffect(() => {
     var myChart = echarts.init(divRef.current)
-    myChart.setOption(
+    setInterval(() => {
+      const newData = [
+        { name: '2015', h1: Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+        { name: '2016', h1:  Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+        { name: '2017', h1:  Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+        { name: '2018', h1:  Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+        { name: '2019', h1:  Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+        { name: '2020', h1:  Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+        { name: '2021', h1:  Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+        { name: '2022', h1:  Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+        { name: '2023', h1:  Math.random()/10, h2:  Math.random()/10, h3:  Math.random()/10, h4:  Math.random()/10, h5:  Math.random()/10 },
+      ]
+      x(newData)
+    }, 3000)
+  }, [])
+  const x = (data: any) => {
+    myChart.current.setOption(
       createEchartsOptions({
         legend: {
           bottom: px(10),
@@ -25,7 +53,7 @@ export const Chart3 = () => {
         xAxis: {
           type: 'category',
           boundaryGap: false,
-          data: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
+          data: data.map((i: any) => i.name),
           splitLine: { show: true, lineStyle: { color: '#073E78' } },
           axisTick: { show: false },
           axisLine: { show: false },
@@ -43,27 +71,27 @@ export const Chart3 = () => {
           {
             name: '抢劫',
             type: 'line',
-            data: [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09].reverse(),
+            data: data.map((i: any) => i.h1).reverse(),
           },
           {
             name: '醉驾',
             type: 'line',
-            data: [0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1].reverse(),
+            data: data.map((i: any) => i.h2).reverse(),
           },
           {
             name: '盗窃',
             type: 'line',
-            data: [0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11].reverse(),
+            data: data.map((i: any) => i.h3).reverse(),
           },
           {
             name: '故意杀人',
             type: 'line',
-            data: [0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12].reverse(),
+            data: data.map((i: any) => i.h4).reverse(),
           },
           {
             name: '故意伤人',
             type: 'line',
-            data: [0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13].reverse(),
+            data: data.map((i: any) => i.h5).reverse(),
           },
         ].map(obj => ({
           ...obj,
@@ -73,6 +101,10 @@ export const Chart3 = () => {
         })),
       })
     )
+  }
+    useEffect(() => {
+      myChart.current = echarts.init(divRef.current)
+      x(data)
   }, [])
 
   return (
