316bb5f67a0df754dfca48437fb850ad62030a4f
shyhhh <hhhtheshy@gmail.com>
2023-03-14 21:08:33 +0800

    修改 13 数据

--- src/components/chart-13.tsx
+++ src/components/chart-13.tsx
@@ -1,23 +1,34 @@
 import React, { useEffect, useRef } from 'react'
 import * as echarts from 'echarts'
 import { createEchartsOptions } from '../shared/create-echarts-options'
+import { faker } from '@faker-js/faker'
+const data = (value, name) => ({ value, name })
+const nameChart13 = ['东岗路', '段家滩', '雁北', '五泉山', '中山路', '庆阳路', '武都路', '酒泉路', '天水路']
 
+let data1 = []
+const getData = (names) => {
+  for (let i = 0; i < names.length; i++) {
+    data1.push(data(faker.finance.amount() / 10000, names[i]))
+  }
+  return data1
+}
 export const Chart13 = () => {
   const divRef = useRef(null)
-  const data = [
-    { value: 0.08, name: '东岗路' },
-    { value: 0.06, name: '段家滩' },
-    { value: 0.11, name: '雁北' },
-    { value: 0.09, name: '五泉山' },
-    { value: 0.12, name: '中山路' },
-    { value: 0.06, name: '庆阳路' },
-    { value: 0.08, name: '武都路' },
-    { value: 0.08, name: '酒泉路' },
-    { value: 0.08, name: '天水路' },
-  ]
+  const myChart = useRef(null)
+  const data = getData(nameChart13)
   useEffect(() => {
     var myChart = echarts.init(divRef.current)
-    myChart.setOption(
+    const id = setInterval(() => {
+      data1 = []
+      const newData = getData(nameChart13)
+      x(newData)
+    }, 1600)
+    return () => {
+      clearInterval(id)
+    }
+  }, [])
+  const x = (data) => {
+    myChart.current.setOption(
       createEchartsOptions({
         xAxis: {
           data: data.map(i => i.name),
@@ -68,6 +79,10 @@ export const Chart13 = () => {
         ],
       })
     )
+  }
+  useEffect(() => {
+    myChart.current = echarts.init(divRef.current)
+    x(data)
   }, [])
 
   return <div ref={divRef} className="chart"></div>
